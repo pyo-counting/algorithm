7bca935d700c9f697fe3e140ddd6d979eed6ff57
pyo_counting <pyo_counting@kakao.com>
2022-02-21 13:28:28 +0900

    Add problem 1039

--- baekjoon/graph_theory/problem_1039.java
+++ baekjoon/graph_theory/problem_1039.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class problem_1039 {
+public class Problem_1039 {
+
     public static int result;
 
     public static void main(String[] args) throws IOException {
@@ -15,29 +16,28 @@ public class problem_1039 {
         StringBuffer N = new StringBuffer(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         result = Integer.parseInt(N.toString());
-
-        DFS(N, 1, 1, K + 1);
+        DFS(N, K);
         System.out.println(result);
     }
 
-    public static void DFS(StringBuffer number, int i, int j, int k) {
-        if(k == 0)
-            return;
-
-        char temp = number.charAt(i - 1);
-        number.setCharAt(i - 1, number.charAt(j - 1));
-        number.setCharAt(j - 1, temp);
-
-        if(Integer.parseInt(number.toString()) > result) {
-            result = Integer.parseInt(number.toString());
-            System.out.println(result);
-        }
-
-        for(int J = i + 1; J < number.length(); J++){
-            for(int I = i; I < J; I++){
-                System.out.println(I + " " + J + " " + k);
-                DFS(number, I, J, k - 1);
+    public static void DFS(StringBuffer number, int k) {
+        if (k == 0) {
+            if (Integer.parseInt(number.toString()) > result)
+                result = Integer.parseInt(number.toString());
+        } else {
+            result = -1;
+            for (int J = 1; J < number.length(); J++) {
+                for (int I = 0; I < J; I++) {
+                    StringBuffer changedNumber = new StringBuffer(number.toString());
+                    char temp = changedNumber.charAt(I);
+                    changedNumber.setCharAt(I, changedNumber.charAt(J));
+                    changedNumber.setCharAt(J, temp);
+                    if (changedNumber.charAt(0) != '0')
+                        DFS(changedNumber, k - 1);
+                }
             }
         }
     }
 }
+
+
